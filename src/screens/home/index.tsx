26947ece0977482a9a577d4b68f163d861e49215
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2023-11-12 18:11:25 +0530

    implement check update on button press

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,21 +1,83 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, {useEffect, useState} from 'react';
-import {FlatList, StyleSheet, Text, View} from 'react-native';
-//import {data} from '../../data';
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {data as embeddedData} from '../../data';
 
 export default function Home() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(embeddedData);
+  const [loading, setLoading] = useState(false);
+  const [loadingText, setLoadingText] = useState('Loading ...');
+
+  /* Get latest DB from the disk */
+  const getSavedVersion = async () => {
+    const savedData = await AsyncStorage.getItem('latestDB');
+    if (savedData) {
+      setData(JSON.parse(savedData));
+    }
+  };
 
   useEffect(() => {
-    const getLatestVersion = async () => {
-      const savedData = await AsyncStorage.getItem('latestDB');
-      if (savedData) {
-        setData(JSON.parse(savedData));
-      }
-    };
-
-    getLatestVersion();
+    getSavedVersion();
   }, []);
+
+  /* Get latest DB from the server */
+  const getLatestVersion = async () => {
+    const res = await fetch('http://192.168.1.40:3000/api/latest-db');
+    const {data} = await res.json();
+
+    /* Refresh the UI */
+    setData(data);
+
+    /* Save the data */
+    console.log('Saving latest version...');
+    await AsyncStorage.setItem('latestDB', JSON.stringify(data));
+    console.log('Saved latest version.');
+  };
+
+  /* To compare two versions */
+  const compareStr = (a: string, b: string) => {
+    return a.localeCompare(b, undefined, {numeric: true, sensitivity: 'base'});
+  };
+
+  /* Callback for the update button */
+  const handleFetchUpdate = async () => {
+    setLoading(true);
+    let updateAvailable = false;
+
+    /* Check the latest version */
+    const res = await fetch('http://192.168.1.40:3000/api/latest-version');
+    const {version} = await res.json();
+
+    /* Check existing version */
+    const savedVersion = await AsyncStorage.getItem('version');
+    if (savedVersion) {
+      console.log('Saved Version:', savedVersion);
+      console.log('Latest Version:', version);
+      setLoadingText(`Latest: ${version}, Saved: ${savedVersion}`);
+      /* Check if a later version is available */
+      if (compareStr(savedVersion, version) === -1) {
+        updateAvailable = true;
+      }
+    } else {
+      console.log('There is no saved version');
+      updateAvailable = true;
+    }
+
+    if (updateAvailable) {
+      setLoadingText('Fetching latest version ...');
+      await getLatestVersion();
+      await AsyncStorage.setItem('version', version);
+    }
+    setLoading(false);
+    setLoadingText('Loading ...');
+  };
 
   const renderItem = ({item}: {item: any}) => (
     <View style={styles.container}>
@@ -39,9 +101,15 @@ export default function Home() {
   return (
     <View style={{flex: 1}}>
       <FlatList data={data} renderItem={renderItem} />
-      <View style={styles.fab}>
+      <TouchableOpacity style={styles.fab} onPress={handleFetchUpdate}>
         <Text style={styles.textFab}>Update</Text>
-      </View>
+      </TouchableOpacity>
+      {loading && (
+        <View style={styles.loaderContainer}>
+          <ActivityIndicator size={'large'} />
+          <Text style={styles.textLoading}>{loadingText}</Text>
+        </View>
+      )}
     </View>
   );
 }
@@ -54,6 +122,16 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#ccc',
     borderRadius: 8,
+  },
+  loaderContainer: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: '#ffffffdd',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textLoading: {
+    fontSize: 16,
+    marginTop: 10,
   },
   elemContainer: {
     flexDirection: 'row',
